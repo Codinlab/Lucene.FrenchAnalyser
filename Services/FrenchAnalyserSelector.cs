1a3ed8ff11641bc591c9422532d217bf9a896598
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-04-04 20:50:03 +0200

    Updating for the soon-to-be-released Orchard 1.10.2 with user-selectable Lucene analyzers

--- Services/FrenchAnalyserSelector.cs
+++ Services/FrenchAnalyserSelector.cs
@@ -6,6 +6,8 @@ using System.Web;
 
 namespace Lucene.FrenchAnalyser.Services {
     public class FrenchAnalyserSelector : ILuceneAnalyzerSelector {
+        public string Name => "French";
+
         public LuceneAnalyzerSelectorResult GetLuceneAnalyzer(string indexName) {
             return new LuceneAnalyzerSelectorResult {
                 Priority = 0,
